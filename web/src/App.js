6a45fccc662df766bd28206920ab386325e0fc14
Pratham <prathusaf@gmail.com>
2023-03-13 06:49:10 -0500

    Test

--- web/src/App.js
+++ web/src/App.js
@@ -1,27 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import Doctor from "./pages/doctor";
 import Patient from "./pages/patient";
-import Home from "./pages/home"
+import Home from "./pages/home";
 import PatientInfo from "./pages/doctor_patient";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      {/* <BrowserRouter 
-        // basename={'MadHacks-2023/'}
-      > */}
-      <Navbar />
-      <Router>
-        <Routes>
-          <Route exact path="/MadHacks-2023/" element={<Home />} />
-          <Route path="/MadHacks-2023/doctor" element={<Doctor />} />
-          <Route path="/MadHacks-2023/patient" element={<Patient />} />
-          <Route path="/MadHacks-2023/doctor_patient" element={<PatientInfo />} />
-        </Routes>
-      </Router>
-      {/* </BrowserRouter> */}
+      <BrowserRouter basename="/MadHacks-2023">
+        <Navbar />
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/doctor" element={<Doctor />} />
+            <Route path="/patient" element={<Patient />} />
+            <Route path="/doctor_patient" element={<PatientInfo />} />
+          </Routes>
+        </Router>
+      </BrowserRouter>
     </>
   );
 }
